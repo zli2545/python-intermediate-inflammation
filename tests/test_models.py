6f497d28c35202bad98d78392ef59cdeb0e31d80
zhli_psy <zli2545@wisc.edu>
2025-03-26 10:51:21 -0500

    added defensive programming

--- tests/test_models.py
+++ tests/test_models.py
@@ -50,26 +50,31 @@ def test_daily_min_string():
 @pytest.mark.parametrize(
     "test, expected, expect_raises",
     [
-        # previous test cases here, with None for expect_raises, except for the next one - add ValueError
-        # as an expected exception (since it has a negative input value)
-            (
-            [[-1, 2, 3], [4, 5, 6], [7, 8, 9]],
-            [[0, 0.67, 1], [0.67, 0.83, 1], [0.78, 0.89, 1]],
-            ValueError,
+        (
+            'hello',
+            None,
+            TypeError,
         ),
         (
-                [[1, 2, 3], [4, 5, 6], [7, 8, 9]],
-                [[0.33, 0.67, 1], [0.67, 0.83, 1], [0.78, 0.89, 1]],
-                None,
+            3,
+            None,
+            TypeError,
         ),
+        (
+            [[1, 2, 3], [4, 5, 6], [7, 8, 9]],
+            [[0.33, 0.67, 1], [0.67, 0.83, 1], [0.78, 0.89, 1]],
+            None,
+        )
     ])
 def test_patient_normalise(test, expected, expect_raises):
     """Test normalisation works for arrays of one and positive integers."""
-
+    if isinstance(test, list):
+        test = np.array(test)
     if expect_raises is not None:
         with pytest.raises(expect_raises):
-            result = patient_normalise(np.array(test))
-            npt.assert_allclose(result, np.array(expected), rtol=1e-2, atol=1e-2)
+          result = patient_normalise(test)
+          npt.assert_allclose(result, np.array(expected), rtol=1e-2, atol=1e-2)
+
     else:
-        result = patient_normalise(np.array(test))
+        result = patient_normalise(test)
         npt.assert_allclose(result, np.array(expected), rtol=1e-2, atol=1e-2)
