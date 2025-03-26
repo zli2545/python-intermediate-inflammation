7763d1b59fe1c9135772f50d87e2141843e8be06
zhli_psy <zli2545@wisc.edu>
2025-03-26 10:36:20 -0500

    added new normalization functio and the test function for it

--- inflammation/models.py
+++ inflammation/models.py
@@ -41,3 +41,7 @@ def daily_min(data):
     """
     return np.min(data, axis=0)
 
+def patient_normalise(data):
+    """Normalise patient data from a 2D inflammation data array."""
+    max = np.max(data, axis=1)
+    return data / max[:, np.newaxis]
