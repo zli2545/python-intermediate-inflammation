a6dca3ee67ba4df822f8f8ab1cd373df55aeedc0
zhli_psy <zli2545@wisc.edu>
2025-03-27 09:15:58 -0500

    add class

--- inflammation/compute_data.py
+++ inflammation/compute_data.py
@@ -14,11 +14,7 @@ def analyse_data(data_dir):
     Gets all the inflammation data from CSV files within a directory,
     works out the mean inflammation value for each day across all datasets,
     then plots the graphs of standard deviation of these means."""
-    data_file_paths = glob.glob(os.path.join(data_dir, 'inflammation*.csv'))
-    if len(data_file_paths) == 0:
-        raise ValueError(f"No inflammation data CSV files found in path {data_dir}")
-    data = map(models.load_csv, data_file_paths)
-
+    data = load_inflammation_data(data_dir)
 
     means_by_day = map(models.daily_mean, data)
     means_by_day_matrix = np.stack(list(means_by_day))
@@ -29,3 +25,28 @@ def analyse_data(data_dir):
         'standard deviation by day': daily_standard_deviation,
     }
     views.visualize(graph_data)
+
+def load_inflammation_data(data_dir):
+    data_file_paths = glob.glob(os.path.join(data_dir, 'inflammation*.csv'))
+    if len(data_file_paths) == 0:
+        raise ValueError(f"No inflammation data CSV files found in path {data_dir}")
+    data = map(models.load_csv, data_file_paths)
+
+    # return list of 2D Numpy arrays of inflammation data
+    return data
+
+class CSVDataSource(object):
+    """
+    Loads inflammation data from a CSV file folder.
+    """
+    def __init__(self, dir_path):
+        self.path = dir_path
+
+    def load_inflammation_data(self):
+        data_file_paths = glob.glob(os.path.join(self.data_dir, 'inflammation*.csv'))
+        if len(data_file_paths) == 0:
+            raise ValueError(f"No inflammation data CSV files found in path {self.data_dir}")
+        data = map(models.load_csv, data_file_paths)
+
+        # return 2D Numpy arrays of inflammation data
+        return list(data)
